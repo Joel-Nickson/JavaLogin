c03e37b506e877380521157c7223ce211eb4a04a
Joel Nickson <joel2000nickson@gmail.com>
2021-01-31 13:49:17 +0530

    added the first page

--- JavaLogin/JavaLogin.java
+++ JavaLogin/JavaLogin.java
@@ -3,12 +3,63 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class JavaLogin{
+	static String insert="insert into logintable(name,username,password) ";
+	static String select="select * from logintable ";
+	static JFrame f;
+	static JButton signin ,sinbutton ,signup ,supbutton ,next ,done ,retry;
+	static JTextField name ,user ,pass;
+	static JPanel s_up ,s_uppage ,s_upbutton ,s_in ,s_inpage ,s_inbutton ,s_login ,s_loginbutton ,newJpanel;
+	static JPanel s_login_one ;
+	static ResultSet rs;
+	static String database_name, username, password ,n ,u ,p;
+
 	JavaLogin(){
+		f = new JFrame("Framey");
+
+		name = new JTextField("",13);
+		user = new JTextField("",13);
+		pass = new JTextField("",13);
+
+		signin = new JButton("Sign In");
+		signin.addActionListener(this);
+		signup = new JButton("Sign Up");
+		signup.addActionListener(this);
+		next = new JButton("Sign in");
+		next.addActionListener(this);
+		done = new JButton("Sign up");
+		done.addActionListener(this);
+		retry = new JButton("retry");
+		retry.addActionListener(this);
 		
+		newJpanel = Login_one(); // replace the Login_one() with your methods to test them out
+		
+		f.add(newJpanel);
+
+		f.setLayout(new FlowLayout());
+        	f.setSize(300,200);
+        	f.setVisible(true);
+        	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	}
-	
-	
-	public static void main(String args[]){
+	static JPanel Login_one(){
 		
+		s_login_one = new JPanel();
+		s_loginbutton = new JPanel();
+		
+		user = new JTextField("Username");
+		JLabel label = new JLabel("\t BLA BLA BLA ");
+
+		s_login_one.setLayout(new BorderLayout());
+		s_loginbutton.setLayout(new FlowLayout());
+
+		s_loginbutton.add(next);
+		
+		s_login_one.add(label,BorderLayout.CENTER);
+		s_login_one.add(s_loginbutton,BorderLayout.SOUTH);
+		return s_login_one;
+		
+	}	
+	public static void main(String args[]){
+		new JavaLogin();
 	}
 }
